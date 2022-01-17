2e56918d8bc2c5a5f5084d9c703be2198300cab2
kev-ye <kangkai.ye@hotmail.com>
2022-01-17 18:38:17 +0100

    front add component

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
+import { ProfilComponent } from './profil/profil.component';
+import { Login2Component } from './login2/login2.component';
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginComponent,
+    ProfilComponent,
+    Login2Component,
+    ChatComponent
   ],
   imports: [
     BrowserModule,
