e0d72455acbdb251cdf2cad71e19fcd8ced257c3
Benjamin Sellem <besellem@student.42.fr>
2022-02-20 15:11:14 +0100

    game in process

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { UserPreferenceComponent } from './user-preference/user-preference.compo
 import { SharedMaterialModule } from './common/shared-material.module';
 import { EditableItemComponent } from './editable-item/editable-item.component';
 import { TwoFactorComponent } from './two-factor/two-factor.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { TwoFactorComponent } from './two-factor/two-factor.component';
     EditableItemComponent,
     UserSubscriptionComponent,
     TwoFactorComponent,
+    GameComponent,
   ],
   imports: [
     BrowserModule,
