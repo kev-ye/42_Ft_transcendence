49144be365da5b3849219de9617cc27179884607
Benjamin Sellem <besellem@student.42.fr>
2022-02-28 16:32:19 +0100

    small debugs on game engine

--- front-end/src/app/game/game.component.ts
+++ front-end/src/app/game/game.component.ts
@@ -7,6 +7,9 @@ import { Component, HostListener, Inject, Input, OnInit, Output } from '@angular
 })
 export class GameComponent implements OnInit {
 
+	private gameStarted: boolean = false;
+	fillColor = 'rgb(20, 20, 20)';
+
 	game = {
 		WIDTH:  700,
 		HEIGHT: 400
@@ -19,17 +22,17 @@ export class GameComponent implements OnInit {
 		RADIUS: 3,
 		SPEED: 10
 	}
-	
+
 	ball_t = {
 		RADIUS: 7,
-		SPEED: 4,
+		SPEED: 6,
 		X: this.game.WIDTH / 2,
 		Y: this.game.HEIGHT / 2
 	}
 
 	ball = {
-		x: 0,
-		y: 0,
+		x: this.ball_t.X,
+		y: this.ball_t.Y,
 		xIncrement: 0,
 		yIncrement: 0,
 	}
@@ -67,20 +70,28 @@ export class GameComponent implements OnInit {
 
 	resetBall() : void {
 		this.ball.x = this.ball_t.X
-		this.ball.y = Math.random() * this.game.HEIGHT
-		this.ball.xIncrement = this.ball_t.SPEED * (Math.random() < 0.5 ? -1 : 1)
-		this.ball.yIncrement = this.ball_t.SPEED * (Math.random() < 0.5 ? -1 : 1)
+		this.ball.y = Math.random() * (this.game.HEIGHT - this.ball_t.RADIUS)
+		if (this.ball.y < this.ball_t.RADIUS)
+			this.ball.y = this.ball_t.RADIUS
+
+		this.ball.xIncrement = this.ball_t.SPEED * (Math.random() < 0.5 ? -1 : 1);
+		this.ball.yIncrement = this.ball_t.SPEED * Math.random();
+		if (this.ball.yIncrement < this.ball_t.SPEED)
+			this.ball.yIncrement = this.ball_t.SPEED / 2;
 	}
-	
+
 	start() : void {
-		this.resetBall()
-		window.requestAnimationFrame(() => this.moveBall());
+		if (!this.gameStarted) {
+			this.gameStarted = true
+			this.resetBall()
+			window.requestAnimationFrame(() => this.moveBall());
+		}
 	}
 
 	@HostListener("window:keydown", ["$event"])
   	onKeyDown(e: any) {
 		let threshold: number = 0
-		
+
 		if (e.code === "ArrowUp") {
 			threshold = -this.paddle.SPEED
 		}
@@ -129,27 +140,27 @@ export class GameComponent implements OnInit {
 	paddleCollision(x: number, y: number) : boolean {
 
 		// check if the ball is aligned with one of the paddle
-		const rightPaddleCollision: boolean = 
+		const rightPaddleCollision: boolean =
 			y >= this.player1.y &&
 			y <= (this.player1.y + this.paddle.HEIGHT);
 
 		const leftPaddleCollision: boolean =
 			y >= this.player2.y &&
 			y <= (this.player2.y + this.paddle.HEIGHT);
-		
+
 		/*
 			check if the ball is in the paddle area,
 			and that the ball is actually in front of the corresponding paddle.
 			(means that we have to bounce it)
 		*/
-		const rightCollision: boolean = 
-			   x - (this.paddle.PADDING + this.paddle.WIDTH) < 0 &&
-			this.ball.x - (this.paddle.PADDING + this.paddle.WIDTH) > 0 &&
+		const rightCollision: boolean =
+					  x - (this.paddle.PADDING + this.paddle.WIDTH) <= 0 &&
+			this.ball.x - (this.paddle.PADDING + this.paddle.WIDTH) >  0 &&
 			rightPaddleCollision;
 
 		const leftCollision: boolean =
-			   x + (this.paddle.PADDING + this.paddle.WIDTH) > this.game.WIDTH &&
-			this.ball.x + (this.paddle.PADDING + this.paddle.WIDTH) < this.game.WIDTH &&
+					  x + (this.paddle.PADDING + this.paddle.WIDTH) >= this.game.WIDTH &&
+			this.ball.x + (this.paddle.PADDING + this.paddle.WIDTH) <  this.game.WIDTH &&
 			leftPaddleCollision;
 
 		return rightCollision || leftCollision
@@ -169,15 +180,23 @@ export class GameComponent implements OnInit {
 			// bounce off the paddles
 			if (this.paddleCollision(future_x, future_y))
 				this.ball.xIncrement = -this.ball.xIncrement
-			
+
 			// bounce off top and bottom walls
 			if (this.wallCollision(future_y))
 				this.ball.yIncrement = -this.ball.yIncrement
-	
+
 			this.ball.x += this.ball.xIncrement
 			this.ball.y += this.ball.yIncrement
 		}
 
 		window.requestAnimationFrame(() => this.moveBall());
+	}
+
+	changeColor() : void {
+		const r = Math.floor(Math.random() * 256);
+		const g = Math.floor(Math.random() * 256);
+		const b = Math.floor(Math.random() * 256);
+		this.fillColor = `rgb(${r}, ${g}, ${b})`;
+		console.log(this.fillColor);		
 	}
 }
