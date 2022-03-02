31120ec364dc0b9a4ee77651ab6ea8ee0ac7f59f
Paul Poinsinet de Sivry <paul_poinsinet@hotmail.com>
2022-03-02 16:02:37 +0100

    dialogs clean

---
+++ front-end/src/app/user/dialogs/dialog-change-username.component.ts
@@ -0,0 +1,63 @@
+import { HttpClient } from "@angular/common/http";
+import { Component, ElementRef, Inject, OnInit, ViewChild } from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { GlobalConsts } from "src/app/common/global";
+
+@Component({
+    templateUrl: './dialog-change-username.html'
+  })
+  export class DialogChangeUsername implements OnInit {
+  
+    constructor(private http: HttpClient, @Inject(MAT_DIALOG_DATA) private data: any, private dialogRef: MatDialogRef<DialogChangeUsername>) {}
+  
+    change: boolean = false;
+    userID : string = "";
+  
+    ngOnInit(): void {
+        this.userID = this.data.id;
+    }
+  
+    @ViewChild('statusText') private statusText: ElementRef<HTMLDivElement>;
+  
+    changeUsername(newUsername: string) {
+      this.http.put(`${GlobalConsts.userApi}/user/update`, {id: this.userID, name: newUsername}, {withCredentials: true}).subscribe({
+        next: data => {
+          console.log("Changed successfully username");
+          this.dialogRef.close(true);
+        },
+        error : data => {
+          console.log("Could not change username");
+        }
+      })
+    }
+  
+    inputEvent(username: string) {
+  
+      if (username)
+      {
+        //look for username in database and see if available
+        this.http.get(`${GlobalConsts.userApi}/user/name/` + username, {withCredentials: true}).subscribe({
+          next: (data: any) => {
+            if (!data)
+            {
+              this.statusText.nativeElement.textContent = username + " is available";
+              this.change = true;
+            }
+            else
+            {
+              this.change = false;
+              this.statusText.nativeElement.textContent = username + " is not available";
+            }
+          },
+          error: data => {
+            this.statusText.nativeElement.textContent = "Could not check for username";
+            this.change = false;
+          }
+        });
+      } else {
+        this.change = false;
+        this.statusText.nativeElement.textContent = "";
+      }
+  
+    }
+  }
